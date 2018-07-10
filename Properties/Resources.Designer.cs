bdc97af8c58922b551822d8107381e64f20d5b24
Oleksandr Goncharov <vorachnog@gmail.com>
2018-07-10 23:51:00 +0300

    Add icon for modern Windows

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -79,5 +79,15 @@ namespace AutoMova.Properties {
                 return ((System.Drawing.Icon)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon icon10 {
+            get {
+                object obj = ResourceManager.GetObject("icon10", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
     }
 }
