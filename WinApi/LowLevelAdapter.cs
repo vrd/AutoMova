f380d2793c09a07e633f46af80046c941e120e1b
Oleksandr Goncharov <vorachnog@gmail.com>
2018-07-30 16:21:24 +0300

    Stylefix

--- WinApi/LowLevelAdapter.cs
+++ WinApi/LowLevelAdapter.cs
@@ -192,8 +192,7 @@ namespace AutoMova.WinApi
                 if (key.Value)
                 {
                     PressPressedKey(key.Key);
-                }
-                
+                }                
             }
         }
 
